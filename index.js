31acc1fbbaee3137015ca124e9be6d30450de4cd
mamark90 <mamark90@gmail.com>
2021-07-16 11:09:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.getElementById('main').remove();document.createElement('h1')
+
+let newHeader=document.createElement('h1')
+newHeader.id='victory'
+newHeader.innerHTML	= 'Mark is the champion'
